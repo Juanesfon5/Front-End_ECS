241ba0bb2612df0c9559e1a154180c86ed23aeaa
josearangos <jose.arangos@udea.edu.co>
2018-11-15 09:55:11 -0500

    test formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
@@ -10,10 +10,31 @@ import { FormSectionComponent } from "./components/form-section/form-section.com
 import { PeopleSectionComponent } from "./components/people-section/people-section.component";
 import { CodeManagementComponent } from "./components/code-management/code-management.component";
 import { CensusMetricsComponent } from "./components/census-metrics/census-metrics.component";
-import { FormsModule } from '@angular/forms';
-import { HomePageComponent } from './components/home-page/home-page.component';
+import { FormsModule } from "@angular/forms";
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { FormlyModule } from "@ngx-formly/core";
+import { FormlyBootstrapModule } from "@ngx-formly/bootstrap";
+import { ArrayTypeComponent } from "./array.type";
 
 //Libreria de diseño de formulario
+
+export function minlengthValidationMessage(err, field) {
+  return `Should have atleast ${field.templateOptions.minLength} characters`;
+}
+
+export function maxlengthValidationMessage(err, field) {
+  return `This value should be less than ${
+    field.templateOptions.maxLength
+  } characters`;
+}
+
+export function minValidationMessage(err, field) {
+  return `This value should be more than ${field.templateOptions.min}`;
+}
+
+export function maxValidationMessage(err, field) {
+  return `This value should be less than ${field.templateOptions.max}`;
+}
 
 @NgModule({
   declarations: [
@@ -25,13 +46,49 @@ import { HomePageComponent } from './components/home-page/home-page.component';
     PeopleSectionComponent,
     CodeManagementComponent,
     CensusMetricsComponent,
-    HomePageComponent
+    HomePageComponent,
+    ArrayTypeComponent
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    FormlyBootstrapModule,
+    FormlyModule.forRoot({
+      validationMessages: [
+        { name: "required", message: "This field is required" },
+        { name: "minlength", message: minlengthValidationMessage },
+        { name: "maxlength", message: maxlengthValidationMessage },
+        { name: "min", message: minValidationMessage },
+        { name: "max", message: maxValidationMessage }
+      ],
+      types: [
+        { name: "string", extends: "input" },
+        {
+          name: "number",
+          extends: "input",
+          defaultOptions: {
+            templateOptions: {
+              type: "number"
+            }
+          }
+        },
+        {
+          name: "integer",
+          extends: "input",
+          defaultOptions: {
+            templateOptions: {
+              type: "number"
+            }
+          }
+        },
+        { name: "object", extends: "formly-group" },
+        { name: "boolean", extends: "checkbox" },
+        { name: "array", component: ArrayTypeComponent },
+        { name: "enum", extends: "select" }
+      ]
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form-section/form-section.component.ts
+++ src/app/components/form-section/form-section.component.ts
@@ -1,28 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { FormlyFormOptions, FormlyFieldConfig } from "@ngx-formly/core";
+import { FormlyJsonschema } from "@ngx-formly/core/json-schema";
 
 @Component({
-  selector: 'app-form-section',
-  templateUrl: './form-section.component.html',
-  styleUrls: ['./form-section.component.scss']
+  selector: "app-form-section",
+  templateUrl: "./form-section.component.html",
+  styleUrls: ["./form-section.component.scss"]
 })
 export class FormSectionComponent implements OnInit {
-  preguntas = [
-    {
-    "Name" : "Alfreds Futterkiste",
-    "Country" : "Germany",
-    "City" : "Berlin"
-    },
-    {
-      "Name" : "Alfreds Futterkiste",
-      "Country" : "Germany",
-      "City" : "Berlin"
-    }
+  form = new FormGroup({});
+  model: any = {
+    1: "Comuna",
+    2: "Numero de orden",
+    3: "",
+    4: "Piso",
+    5: "Nombre",
+    multipleChoicesList: "1"
+  };
+  options: FormlyFormOptions = {};
+
+  fields: FormlyFieldConfig[] = [
+    this.formlyJsonschema.toFieldConfig({
+      number: 1,
+      type: "object",
+      properties: {
+        "1": {
+          type: "string",
+          title: "NOMBRE DE LA COMUNA",
+          response: ""
+        },
+        "2": {
+          type: "string",
+          title: "N\u00daMERO DE ORDEN DE LA VIVIENDA",
+          response: ""
+        },
+        "3": {
+          type: "integer",
+          title: "NUMERO DEL DOMICILIARIO",
+          response: ""
+        },
+        "4": {
+          type: "string",
+          title: "PISO",
+          response: ""
+        },
+        "5": {
+          type: "string",
+          title: "NOMBRE DE LA CALLE O CAMINO",
+          response: ""
+        },
+        multipleChoicesList: {
+          type: "array",
+          title: "A multiple choices list",
+          items: {
+            type: "string",
+            enum: ["foo", "bar", "fuzz", "qux", "kappa"]
+          },
+          uniqueItems: true,
+          add: null
+        }
+      }
+    })
   ];
-  
-  constructor() { }
 
-  ngOnInit() { 
-    
+  constructor(private formlyJsonschema: FormlyJsonschema) {}
+
+  ngOnInit() {}
+
+  submit() {
+    alert(JSON.stringify(this.model));
   }
-
 }
